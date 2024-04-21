6bb9e330dedbb8cf7002bfc219edd91ea562f6db
Dong, Eric <ericdo16@outlook.com>
2024-04-21 01:09:51 -0400

    Initial commit

---
+++ src/workouts/legPress.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+
+export function LegPress() {
+    const [weight, setWeight] = useState("");
+    const [reps, setReps] = useState("");
+    const [sets, setSets] = useState("");
+
+    const [outputText, setOutputText] = useState("");
+
+    function getOutputText() {
+        return `Today you did ${sets} sets of ${reps} reps of leg press at ${weight} lbs.`
+    }
+
+    function onSubmit(e) {
+        e.preventDefault();
+
+        setOutputText(getOutputText());
+    }
+
+    return (
+        <div>
+            <form onSubmit={onSubmit}>
+                <div>
+                    <label>Weight: </label>
+                    <input type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+                    <label>lbs</label>
+                </div>
+                <div>
+                    <label>Reps: </label>
+                    <input type="number" value={reps} onChange={(e) => setReps(e.target.value)} />
+                    <label></label>
+                </div>
+                <div>
+                    <label>Sets: </label>
+                    <input type="number" value={sets} onChange={(e) => setSets(e.target.value)} />
+                    <label></label>
+                </div>
+                <input type="submit"></input>
+            </form>
+
+            <hr></hr>
+
+            <p>{outputText}</p>
+        </div>
+    );
+}
+
+export default LegPress;
